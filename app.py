f3e836f1b7170e143241c9aa910131a4789810a2
Tohir <thohiru.omoloye@talentbase.ng>
2018-05-15 13:42:45 +0100

    Added settings page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_restful import Resource, Api
 import sqlite3
 from sqlite3 import Error
@@ -22,9 +22,21 @@ def select_all_temp(conn, startDate, endDate):
     if endDate != "0" :
         sql = sql + " and tdate <= ?"
     print(sql)
+
     cur.execute(sql, (startDate, endDate))
     print("executed")
     return cur.fetchall()
+
+def get_recent_setting(conn):
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM settings order by ROWID desc limit 1')
+    return cur.fetchall()
+
+def save_settings(conn, temperature, humidity):
+    cur = conn.cursor()
+    cur.execute('replace into settings values (?,?)', [temperature, humidity])
+
+    return conn.commit()
 
 
 
@@ -51,6 +63,39 @@ def about():
 @app.route('/plot')
 def plot():
     return render_template("plot.html")
+
+@app.route('/settings')
+def settings():
+    # create a database connection
+    database = "/Users/tohir/Downloads/sqlite-autoconf-3230100/project"
+    conn = create_connection(database)
+
+    with conn:
+        print("Get last settings")
+        #select_all_temp(conn)
+        rows = get_recent_setting(conn)
+
+    return render_template("settings.html", temp=rows[0][0], humid=rows[0][1])
+
+@app.route('/result', methods = ['POST', 'GET'])
+def saveSettings():
+
+    # get inputs from form
+
+    temperature = request.form['temperature']
+    humidity = request.form['humidity']
+
+    # create a database connection
+    database = "/Users/tohir/Downloads/sqlite-autoconf-3230100/project"
+    conn = create_connection(database)
+    with conn:
+        print("Send temperature and humidity across")
+        #select_all_temp(conn)
+        rows = save_settings(conn, temperature, humidity)
+
+    #return render_template("settings.html", )
+    return redirect(url_for('settings'))
+
 
 @app.route('/api/get_temp', methods=["GET", "POST"])
 def temperature():

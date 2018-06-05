d5e577ebb62ebba945b46bd3aad3ff2a810486cd
Tohir <thohiru.omoloye@talentbase.ng>
2018-06-05 11:52:08 +0100

    bug fix: adding null to database table

--- AdafruitDHT_11.py
+++ AdafruitDHT_11.py
@@ -11,8 +11,9 @@ while True:
     # humidity = 39
     # temperature = 25
     data = {'humidity':humidity,'temperature':temperature}
-    json_data = json.dumps(data)
-    r = requests.post('http://127.0.0.1:5000/api/post_reading',json_data)
+
+    r = requests.get('http://127.0.0.1:5000/api/post_reading?temperature=' + str(temperature) + '&humidity=' + str(humidity))
+
     print(humidity,temperature)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     time.sleep(5)
